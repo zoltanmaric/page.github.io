c4ed164e16007e1f5d84461967e8430f4e45323a
frogue <zzmaric@gmail.com>
2014-12-13 19:43:40 +0100

    initial import

---
+++ index.php
@@ -0,0 +1,291 @@
+<? include "contact-form/form.contact-form.php"; ?>
+<!DOCTYPE  html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Elegant CV/ Resume Template</title>
+<link href="style/reset.css" rel="stylesheet" type="text/css" />
+<link href="style/main.css" rel="stylesheet" type="text/css" media="screen" />
+<link href="style/print.css" rel="stylesheet" type="text/css" media="print" />
+<link href="style/popup.css" rel="stylesheet" type="text/css" media="screen" />
+
+
+<script src="js/jQuery.min.js"  type="text/javascript"></script>
+<script src="contact-form/contact-form.js"  type="text/javascript"></script>
+<script src="js/popup.js" type="text/javascript"></script>
+
+
+</head>
+<body>
+<!-- Email Confirmation -->
+<div id="successdiv" class="success" style="display:none;"> Your message has been sent successfully! </div>
+
+    
+<div id="container">
+	
+    <!-- header start -->
+    
+    <div id="header-container">
+    	<div class="name"><h1><span class="blue">JOHN</span><span class="grey">FISHERMAN</span></h1></div>
+        <div class="designation"><h1><span class="grey">DESIGNER<span class="blue">+</span>SEO</span></h1></div>
+         <div class="employment">
+         	<div class="box"><h2>Available for Freelance Work!</h2></div>
+         </div>
+    </div>
+    
+    <!-- header end -->
+    
+    <!-- content start -->
+    
+    <div class="maincontent">
+    	
+        <!-- sidebar start -->
+        <div class="leftcontent">
+        		<div class="intro"><p>Lorem ipsum dolor sit amet, consectetuer piscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat laoreet exea erat commodo consequat</p> 
+                </div>
+        		<div class="social-bar">
+                	<ul>
+                    	<li><a href="#"><img src="images/icon-in.jpg" alt="linkedin"  /></a></li>
+                        <li><a href="#"><img src="images/icon-fb.jpg" alt="Faceboook"  /></a></li>
+                        <li><a href="#"><img src="images/icon-twitter.jpg" alt="Twitter"  /></a></li>
+                        <li><a href="#"><img src="images/icon-flickr.jpg" alt="Flickr"  /></a></li>
+                        <li><a href="#"><img src="images/icon-dribbble.jpg" alt="Dribbble"  /></a></li>
+                        <li><a href="#"><img src="images/icon-rss.jpg" alt="Rss" /></a></li>
+                        <li class="contact-button" ><a href="#" id="cformtitle"><img src="images/btn-send.jpg" alt="Send Message" /></a></li>
+                       
+                    </ul>
+                </div>
+                <div class="contct-info">
+                
+                	<strong>Phone</strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;44 23 4567 9908<br />
+					<strong>Web</strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="#">www.johnfman.com</a><br />
+					<strong>Email</strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="mailto:">hello@johnfman.com</a>
+                    
+                </div>
+                
+                <div class="left-row1"><h3>SKILLS</h3></div>
+                <div class="left-row2">
+                      <div class="left-row2-col1">
+                            <ul>
+                              <li>Photoshop</li>
+                              <li> Dreamweaver</li>
+                              <li> HTML5</li>
+                              <li> CSS3</li>
+                              <li> jQuery</li>
+                              <li> Illustrator</li>
+                              <li> Flash </li>
+                            </ul>
+                  </div>
+<div class="left-row2-col2">
+                       		<div class="skills-bars first photoshop"></div>
+                            <div class="skills-bars dreamweaver"></div>
+                            <div class="skills-bars html5"></div>
+                            <div class="skills-bars css3"></div>
+                            <div class="skills-bars jquery"></div>
+                            <div class="skills-bars illustrator"></div>
+                            <div class="skills-bars flash"></div>
+                           
+
+                     </div>
+                </div>
+                
+    			<div class="left-row1">
+                  <h3>REFERENCES</h3>
+                </div>
+                <div class="left-row2">
+                      <div class="left-row2-col1">
+                        <ul>
+                          <li>Shelly Robinson</li>
+                          <li> Umair Waheed</li>
+                          <li> Robert Abram </li>
+                        </ul>
+                  </div>
+                  <div class="left-row2-col2">
+                       <ul>
+                           <li> CEO / Internet Company</li>
+                           <li> Founder / Blend.pk</li>
+                           <li> Director / Loremipsum.com</li>
+                       </ul>
+                  </div>
+          </div>
+                
+                 <div class="left-row1">
+                	  <h3>WHERE I EXCEL</h3>
+                </div>
+                <div class="left-row2">
+                	  <ul>
+                	    <li>Project and Time Management</li>
+                	    <li> Strategic Planning &amp; Successful Execution</li>
+                	    <li> Creative and Out-of-the-box Thinking </li>
+              	    </ul>
+           	   </div>
+            
+       	  <div class="left-row2" style="height:100px;"></div>
+      
+      </div>
+      
+       <!-- sidebar end -->
+      
+       <!-- main content start -->
+       
+        <div class="rightcontent">
+        		<div class="right-row1">
+                	  <h3>WORK EXPERIENCE</h3>
+                </div>
+          		<div class="right-row2">
+            		<div class="right-row2-col1">
+            		  <ul>
+            		    <li><a href="#" class="blue-link">Blend.pk</a></li>
+            		    <li> Principal Designer</li>
+            		    <li> 2009 - To-date</li>
+            		    <li> Full Time / On-site </li>
+          		    </ul>
+           		    </div>
+<div class="right-row2-col2">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit .
+                      </div>
+         	 </div>
+                
+                <div class="right-row3">
+                      <div class="right-row2-col1">
+                        <ul>
+                          <li><a href="#" class="blue-link">Greepit.com</a></li>
+                          <li> SEO Expert</li>
+                          <li> 2007 - 2009</li>
+                          <li> Part Time / Off-site </li>
+                        </ul>
+                  </div>
+                  <div class="right-row2-col2">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                  </div>
+    	       </div>
+	                
+                
+                 <div class="right-row1">
+                  <h3>EDUCATION</h3>
+                </div>
+                <div class="right-row2">
+                      <div class="right-row2-col1">
+                        <ul>
+                          <li><a href="#" class="blue-link">Best University</a></li>
+                          <li> MSC - IT</li>
+                          <li> Design as Major</li>
+                          <li> 2008 / GPA 3.85 </li>
+                        </ul>
+                  </div>
+                  <div class="right-row2-col2">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.				</div>
+                </div>
+                
+          <div class="right-row3">
+	  <div class="right-row2-col1">
+	    <ul>
+	      <li><a href="#" class="blue-link">Design University</a></li>
+	      <li> Bachelor in Arts</li>
+	      <li> Design as Major</li>
+	      <li> 2004 / GPA 3.90 </li>
+	      </ul>
+	    </div>
+			<div class="right-row2-col2">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.		                 </div>
+          </div>
+                
+                
+                <div class="right-row1">
+                  <h3>POTATOES</h3>
+                </div>
+                <div class="right-row2">
+                      <div class="right-row2-col1">
+                        <ul>
+                          <li><a href="#" class="blue-link">President</a></li>
+                          <li> Art Association</li>
+                          <li> 2010 - To-date </li>
+                        </ul>
+                  </div>
+                  <div class="right-row2-col2">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.              </div>
+                </div>
+                
+          <div class="right-row3">
+<div class="right-row2-col1">
+  <ul>
+    <li><a href="#" class="blue-link">Member</a></li>
+    <li> Society of Design</li>
+    <li> 2008 - To-date </li>
+  </ul>
+  </div>
+            <div class="right-row2-col2">
+                     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.
+            </div>
+          </div>
+       
+    </div>
+    
+    
+    <!-- content end -->
+
+	</div>
+	<div class="footer">Copyright Elegant CV Resume 2011. </div>
+</div>
+
+
+<script type="text/javascript" charset="utf-8">
+ 
+$(document).ready(function(){
+ 		
+			$('#loader').css('display','none');
+			var sentParts = window.location.href.toString().split('?');
+			if(sentParts.length > 1)
+			if(sentParts[1] ='sent'){
+			$('#successdiv').css('display','');
+			        setTimeout(function() {
+					    $('#successdiv').fadeOut('fast');
+					}, 3000);
+			}
+			
+			});
+</script>
+            
+<!-- Contact Form Start -->
+<div id="popupContact"> <a id="popupContactClose"><img src="images/x.gif" alt="Close" /></a>
+  <h2>Lets Meet!</h2>
+  <div id="contactArea">
+  <div id="contact-form">
+    <form name="contact" action="<?=$_SERVER['PHP_SELF']?>" method="post" onsubmit="return validateForm();">
+      <table cellpadding="8" cellspacing="10">
+        <tr>
+          <td><label>Your Name <span class="req">*</span></label></td>
+        </tr>
+        <tr>
+          <td><input type="text"  id="sender_name"  name="sender_name" /></td>
+        </tr>
+        <tr>
+          <td><label>Your E-Mail <span class="req">*</span></label></td>
+        </tr>
+        <tr>
+          <td ><input type="text" id="sender_email" name="sender_email" /></td>
+        </tr>
+        <tr>
+          <td><label>Message </label></td>
+        </tr>
+        <tr>
+          <td ><textarea id="sender_message" name="sender_message" cols="" rows="" style="width:380px; height:80px;"></textarea></td>
+        </tr>
+        <tr>
+          <td><input type="submit" name="submitForm" value="Submit" class="button m grey-gloss" />
+            <span id="loader"><img src="images/loader.gif" alt="Progress indicator..." /></span> </td>
+        </tr>
+        <tr>
+          <td colspan="4"><p id="form_errors"></p></td>
+        </tr>
+      </table>
+    </form>
+  </div>
+  </div>
+</div>
+<div id="backgroundPopup"></div>
+<!-- Contact Form End -->
+
+
+</body>
+</html>
